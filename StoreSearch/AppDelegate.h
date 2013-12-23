7bea9c7ab9a6593a76117ff30f0911e2308db8c4
Girl iOS <myladyios@gmail.com>
2013-12-23 20:47:16 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Junnebai on 13-12-23.
+//  Copyright (c) 2013年 GirliOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Junnebai on 13-12-23.
+//  Copyright (c) 2013年 GirliOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
