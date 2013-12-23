e5d1a5b4d2666719385926d3fdbf3c2ea8272e8f
Girl iOS <myladyios@gmail.com>
2013-12-24 00:52:36 +0800

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.h
@@ -0,0 +1,16 @@
+//
+//  SearchResult.h
+//  StoreSearch
+//
+//  Created by Junnebai on 13-12-23.
+//  Copyright (c) 2013年 GirliOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *artistName;
+
+@end
---
+++ StoreSearch/SearchResult.m
@@ -0,0 +1,16 @@
+//
+//  SearchResult.m
+//  StoreSearch
+//
+//  Created by Junnebai on 13-12-23.
+//  Copyright (c) 2013年 GirliOS. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@synthesize name;
+@synthesize artistName;
+
+@end
